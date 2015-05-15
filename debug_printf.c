dc9abb909051208fb09eed7ba08ce0e8c77554e3
Andrea Merello <andrea.merello@gmail.com>
2015-05-15 19:16:59 +0200

    Add serial debug functions

---
+++ debug_printf.c
@@ -0,0 +1,63 @@
+/*
+ * This file is part of UAKEH (Usb Army Knife for Electronic Hacks) project.
+ * Copyright (C) 2015 Andrea Merello <andrea.merello@gmail.com>
+ *
+ *   Mostly based on libopencm3 usart_printf example
+ *   Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>,
+ *   Copyright (C) 2011 Piotr Esden-Tempski <piotr@esden.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <libopencm3/stm32/usart.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <stdio.h>
+#include <errno.h>
+#include "debug_printf.h"
+
+void debug_init()
+{
+	rcc_periph_clock_enable(RCC_GPIOA);
+	rcc_periph_clock_enable(RCC_AFIO);
+	rcc_periph_clock_enable(RCC_USART1);
+
+	gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO_USART1_TX);
+
+	/* Setup UART parameters. */
+	usart_set_baudrate(USART1, 19200);
+	usart_set_databits(USART1, 8);
+	usart_set_stopbits(USART1, USART_STOPBITS_1);
+	usart_set_parity(USART1, USART_PARITY_NONE);
+	usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
+	usart_set_mode(USART1, USART_MODE_TX);
+
+	/* Finally enable the USART. */
+	usart_enable(USART1);
+}
+int _write(int file, char *ptr, int len)
+{
+	int i;
+
+	if (file == 1) {
+		for (i = 0; i < len; i++)
+			usart_send_blocking(USART1, ptr[i]);
+		return i;
+	}
+
+	errno = EIO;
+	return -1;
+}
---
+++ debug_printf.h
@@ -0,0 +1,5 @@
+#ifndef __DEBUG_PRINTF_H__
+#define __DEBUG_PRINTF_H__
+
+void debug_init(void);
+#endif
